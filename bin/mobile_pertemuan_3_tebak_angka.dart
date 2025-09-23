3fe953f0bb773a2a5a618ac6248a8f9de255e27d
AlKhrisW <aldokhrisna@gmail.com>
2025-09-24 06:37:30 +0700

    first commit

---
+++ bin/mobile_pertemuan_3_tebak_angka.dart
@@ -0,0 +1,5 @@
+import 'package:mobile_pertemuan_3_tebak_angka/mobile_pertemuan_3_tebak_angka.dart' as mobile_pertemuan_3_tebak_angka;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mobile_pertemuan_3_tebak_angka.calculate()}!');
+}
