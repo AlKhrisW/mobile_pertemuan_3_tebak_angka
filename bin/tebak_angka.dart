90301e44fb81188c6f1bb305e7c8bf962d8dab12
AlKhrisW <aldokhrisna@gmail.com>
2025-09-24 06:54:58 +0700

    Tambah fitur maksimal percobaan

--- bin/tebak_angka.dart
+++ bin/tebak_angka.dart
@@ -4,20 +4,27 @@ import 'dart:math';
 void main() {
   print('Selamat datang di Game Tebak Angka!');
   int angkaRahasia = Random().nextInt(100) + 1;
+  int percobaan = 0;
+  int maxPercobaan = 10;
   bool menang = false;
 
-  while (!menang) {
+  while (percobaan < maxPercobaan && !menang) {
     stdout.write('Tebakan Anda: ');
     String? input = stdin.readLineSync();
     int tebakan = int.parse(input!);
+    percobaan++;
 
     if (tebakan == angkaRahasia) {
-      print('Selamat! Anda benar.');
+      print('Selamat! Anda benar setelah $percobaan percobaan.');
       menang = true;
     } else if (tebakan < angkaRahasia) {
-      print('Terlalu rendah!');
+      print('Terlalu rendah! Sisa: ${maxPercobaan - percobaan}');
     } else {
-      print('Terlalu tinggi!');
+      print('Terlalu tinggi! Sisa: ${maxPercobaan - percobaan}');
     }
+  }
+
+  if (!menang) {
+    print('Kalah! Angka: $angkaRahasia.');
   }
 }
