2ed68f734815a0b0e4c3393a81e25f0beaa18036
AlKhrisW <aldokhrisna@gmail.com>
2025-09-24 07:39:10 +0700

    Tambah fitur bermain kembali

--- bin/tebak_angka.dart
+++ bin/tebak_angka.dart
@@ -3,28 +3,45 @@ import 'dart:math';
 
 void main() {
   print('Selamat datang di Game Tebak Angka!');
-  int angkaRahasia = Random().nextInt(100) + 1;
-  int percobaan = 0;
-  int maxPercobaan = 10;
-  bool menang = false;
+  bool mainLagi = true;
 
-  while (percobaan < maxPercobaan && !menang) {
-    stdout.write('Tebakan Anda: ');
-    String? input = stdin.readLineSync();
-    int tebakan = int.parse(input!);
-    percobaan++;
+  while (mainLagi) {
+    int angkaRahasia = Random().nextInt(100) + 1; // Angka acak 1-100
+    int percobaan = 0;
+    int maxPercobaan = 10;
+    bool menang = false;
 
-    if (tebakan == angkaRahasia) {
-      print('Selamat! Anda benar setelah $percobaan percobaan.');
-      menang = true;
-    } else if (tebakan < angkaRahasia) {
-      print('Terlalu rendah! Sisa: ${maxPercobaan - percobaan}');
-    } else {
-      print('Terlalu tinggi! Sisa: ${maxPercobaan - percobaan}');
+    print('Saya telah memilih angka antara 1-100. Tebaklah! (Max $maxPercobaan percobaan)');
+
+    while (percobaan < maxPercobaan && !menang) {
+      stdout.write('Tebakan Anda: ');
+      String? input = stdin.readLineSync();
+      
+      try {
+        int tebakan = int.parse(input!);
+        percobaan++;
+
+        if (tebakan == angkaRahasia) {
+          print('Selamat! Anda benar setelah $percobaan percobaan.');
+          menang = true;
+        } else if (tebakan < angkaRahasia) {
+          print('Terlalu rendah! Sisa percobaan: ${maxPercobaan - percobaan}');
+        } else {
+          print('Terlalu tinggi! Sisa percobaan: ${maxPercobaan - percobaan}');
+        }
+      } catch (e) {
+        print('Input tidak valid. Masukkan angka!');
+      }
     }
+
+    if (!menang) {
+      print('Maaf, Anda kalah. Angka rahasia adalah $angkaRahasia.');
+    }
+
+    stdout.write('Main lagi? (y/n): ');
+    String? jawab = stdin.readLineSync()?.toLowerCase();
+    mainLagi = (jawab == 'y');
   }
 
-  if (!menang) {
-    print('Kalah! Angka: $angkaRahasia.');
-  }
+  print('Terima kasih telah bermain!');
 }
